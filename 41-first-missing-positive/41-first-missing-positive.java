08efcb8f7a1b918c71687ed846bc34699dca079e
SarthakTyagi20 <85821901+SarthakTyagi20@users.noreply.github.com>
2022-06-29 16:59:24 +0530

    Time: 5 ms (52.63%), Space: 127.1 MB (59.92%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.java
+++ 41-first-missing-positive/41-first-missing-positive.java
@@ -1,22 +1,55 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        Arrays.sort(nums);
-        int m = 1, i = 0;
+        // Arrays.sort(nums);
+        // int m = 1, i = 0;
         int n = nums.length;
-        while(i<n && nums[i]<=0){
-            i++;
-        }
-        if(i>=n)
-            return m;
-        while(i<n){
-            if(nums[i]==m){
-                m++;
-                i++;
+        for(int i=0; i<n; i++){
+            if(nums[i]<=0 || nums[i]>n){
+                nums[i] = n+1;
             }
-            else{
-                i++;
-            }    
         }
-        return m;
+        for(int i=0; i<n; i++){
+            if(nums[i]<=0 && nums[i]>n){
+                nums[i] = n+1;
+            }
+        }
+        for(int i=0; i<n; i++){
+            int x = Math.abs(nums[i]);
+            if(x<=n){
+                x--;
+                if(nums[x]>0){
+                    nums[x] = -1*nums[x];
+                }
+            }
+        }
+        for(int i=0; i<n; i++){
+            if(nums[i]>0){
+                return i+1;
+            }
+        }
+        // ArrayList<Integer> a= new ArrayList<>();
+        // for(int i=0; i<n; i++){
+        //     a.add(nums[i]);
+        // }
+        // for(int i=1; i<Integer.MAX_VALUE; i++){
+        //     if(!a.contains(i)){
+        //         return i;
+        //     }
+        // }
+        // while(i<n && nums[i]<=0){
+        //     i++;
+        // }
+        // if(i>=n)
+        //     return m;
+        // while(i<n){
+        //     if(nums[i]==m){
+        //         m++;
+        //         i++;
+        //     }
+        //     else{
+        //         i++;
+        //     }    
+        // }
+        return n+1;
     }
 }
