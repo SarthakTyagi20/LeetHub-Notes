6c997263da2d30b6b217a56e9b37a29e1fa89829
SarthakTyagi20 <85821901+SarthakTyagi20@users.noreply.github.com>
2022-07-05 18:02:30 +0530

    Time: 793 ms (6.78%), Space: 206.4 MB (5.03%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
@@ -1,22 +1,34 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
-        Arrays.sort(nums);
+        // Arrays.sort(nums);
+        HashSet<Integer> set = new HashSet<>();
         int n = nums.length, cons = 0, maxl = 0;
-        if(n>0){
-            cons=1;
-            maxl=1;
-        }
-        for(int i=0; i<n-1; i++){
-            if(nums[i+1]==nums[i]+1)
-                cons++;
-            else if(nums[i+1]==nums[i]){
-                continue;
+        for(int i=0; i<n; i++)
+            set.add(nums[i]);
+        for(int i=0; i<n; i++){
+            if(!set.contains(nums[i]-1)){
+                int x = nums[i]+1;
+                while(set.contains(x))
+                    x++;
+                cons = x-nums[i];
+                maxl = Math.max(maxl, cons);
             }
-            else{
-                cons=1;
-            }
-            maxl = Math.max(maxl, cons);
         }
+//         if(n>0){
+//             cons=1;
+//             maxl=1;
+//         }
+        // for(int i=0; i<n-1; i++){
+        //     if(nums[i+1]==nums[i]+1)
+        //         cons++;
+        //     else if(nums[i+1]==nums[i]){
+        //         continue;
+        //     }
+        //     else{
+        //         cons=1;
+        //     }
+        //     maxl = Math.max(maxl, cons);
+        // }
         return maxl;
     }
 }
