0da00d78adba6a51df528b2703a59e5da1d74c78
SarthakTyagi20 <85821901+SarthakTyagi20@users.noreply.github.com>
2022-07-06 23:20:45 +0530

    Time: 1 ms (32.80%), Space: 42.6 MB (43.55%) - LeetHub

---
+++ 27-remove-element/27-remove-element.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int n = nums.length, c=0, idx=-1;
+        for(int i=0; i<n; i++){
+            if(nums[i]==val){
+                c++;
+                if(idx==-1)
+                    idx=i;
+            }
+        }
+        if(c==0)
+            return n;
+        int i=idx, j=idx+1;
+        while(j<n && nums[j]==val)
+            j++;
+        while(j<n && i<n){
+            if(nums[i]==val && nums[j]!=val){
+                nums[i]=nums[j];
+                // System.out.println("1 i:"+i+" j:"+j);
+                j++;
+                while(j<n && nums[j]==val)
+                    j++;
+                i++;
+            }
+            else if(nums[j]!=val){
+                nums[i] = nums[j];
+                // System.out.println("2 i:"+i+" j:"+j);
+                i++;
+                j++;
+            }
+            else{
+                while(j<n && nums[j]==val)
+                    j++;
+                if(j<n){
+                    nums[i] = nums[j];
+                    // System.out.println("3 i:"+i+" j:"+j);
+                    i++;
+                    j++;
+                }
+            }
+        }
+        return n-c;
+    }
+}
