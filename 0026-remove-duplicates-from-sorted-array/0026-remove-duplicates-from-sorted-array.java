328e1b365dc6a3a3d8d2e2128e8e4452d1610039
SarthakTyagi20 <85821901+SarthakTyagi20@users.noreply.github.com>
2022-11-11 17:16:40 +0530

    Time: 2 ms (73.26%), Space: 48.1 MB (19.04%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+//        int c = 0;
+//         if(nums.length>0){
+//             c++;
+//         }
+//         for (int i = 0; i < nums.length - 1; i++) {
+//             int k = 1;
+//             while ((i + k < nums.length) && (nums[i] == nums[i + k])) {
+//                 k++;
+//             }
+//             if(k>1){
+//                 if (i + k < nums.length) {
+// //                    nums[i + 1] = nums[i + k];
+// //                    if (i + k < nums.length-1){
+// //                        nums[i + k]=nums[i + k + 1];
+// //                    }
+//                     for(int j = i+1; j<nums.length-1; j++){
+//                         if(j + k -1 < nums.length)
+//                             nums[j] = nums[j+k-1];
+//                     }
+//                 }
+// //                i = i+k;
+//             }
+// //            else{
+// //                i++;
+// //            }
+//         }
+//         for (int i = 0; i < nums.length - 1; i++){
+//             if (nums[i] > nums[i + 1]){
+//                 break;
+//             }
+//             if((nums[i] != nums[i + 1]) && (nums[i] < nums[i + 1])){
+//                 c++;
+//             }
+//         }
+//             return c;
+        int cur=0, n=nums.length, cnt=0;
+        if(n>0){
+            cnt++;
+        }
+        for(int i=1; i<n; i++){
+            if(nums[i] != nums[cur]){
+                nums[++cur] = nums[i];
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+}
